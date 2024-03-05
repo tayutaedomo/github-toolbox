b6712e79b76c5414a268533714219767c7c65c42
tayutaedomo <tayutaedomo@gmail.com>
2024-03-06 00:51:43 +0900

    Update fetch issues script

--- scripts/fetch_assigned_issues.py
+++ scripts/fetch_assigned_issues.py
@@ -2,14 +2,19 @@ import os
 import sys
 
 from github import Github
-from github.Repository import Repository
 
 
 if __name__ == "__main__":
     g = Github(os.environ["GITHUB_TOKEN"])
 
     repo_name = sys.argv[1]
+    assignee = sys.argv[2]
+
     repo = g.get_repo(repo_name)
     
-    for issue in repo.get_issues(state="closed", assignee="me"):
-        print(issue.title)
+    for issue in repo.get_issues(state="closed", assignee=assignee):
+        title = issue.title
+        state = issue.state
+        created_at = issue.created_at.isoformat()
+        closed_at = issue.closed_at.isoformat() if issue.state == "closed" else ""
+        print(title, state, created_at, closed_at)
